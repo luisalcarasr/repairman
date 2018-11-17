69426158a1948a2d7e6c015fe66dcb1301452e1f
Luis Alcaras <luisalcarasr@gmail.com>
2018-11-16 22:22:50 -0600

    Auth layout was finished.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,54 +1,22 @@
-<!DOCTYPE html>  
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
-<link rel="icon" type="image/png" sizes="16x16" href="http://rodasports.com.mx/www/wp-content/uploads/2015/06/ICON_RDSPRTS_TTR.jpg">
-<title>{{ config('app.name') }} - Sign In</title>
-<!-- Bootstrap Core CSS -->
-<link href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-<!-- animation CSS -->
-<link href="{{ asset('css/animate.css') }}" rel="stylesheet">
-<!-- Custom CSS -->
-<link href="{{ asset('css/style.css') }}" rel="stylesheet">
-<!-- color CSS -->
-<link href="{{ asset('css/colors/default.css') }}" id="theme"  rel="stylesheet">
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="👨🏾‍🔧 System for the maintenance control made with Laravel Framework.">
+    <meta name="author" content="Luis Alcaras">
+    <link rel="icon" type="image/png" sizes="16x16"
+          href="http://rodasports.com.mx/www/wp-content/uploads/2015/06/ICON_RDSPRTS_TTR.jpg">
+    <title>{{ config('app.name') }} - Sign In</title>
+    <!-- CSS Application -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<!-- Preloader -->
-<div class="preloader">
-  <div class="cssload-speeding-wheel"></div>
-</div>
-
 <main>
     @yield('content')
 </main>
-
-
-<!-- jQuery -->
-<script src="{{ asset('plugins/bower_components/jquery/dist/jquery.min.js') }}"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="{{ asset('bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<!-- Menu Plugin JavaScript -->
-<script src="{{ asset('plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js') }}"></script>
-
-<!--slimscroll JavaScript -->
-<script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
-<!--Wave Effects -->
-<script src="{{ asset('js/waves.js') }}"></script>
-<!-- Custom Theme JavaScript -->
-<script src="{{ asset('js/custom.min.js') }}"></script>
-<!--Style Switcher -->
-<script src="{{ asset('plugins/bower_components/styleswitcher/jQuery.style.switcher.js') }}"></script>
+<!-- JavaScript Application -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
