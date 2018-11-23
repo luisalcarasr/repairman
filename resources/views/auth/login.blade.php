4d535baad98551c49e75263d2c839577f204fb44
Luis Alcaras <luisalcarasr@gmail.com>
2018-11-23 16:51:37 -0600

    Sign in view has been migrate from bootstrap to bulma.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,75 @@
 @extends('layouts.auth')
 
 @section('content')
-<section id="wrapper" class="new-login-register">
-      <div class="lg-info-panel">
-            <div class="inner-panel">
-                <div class="lg-content">
-                    <h2>Mantenimiento System</h2>
-                    <p class="text-muted"></p>
+    <div class="container">
+        <section class="columns">
+            <div class="column is-offset-one-quarter is-half">
+                <div class="card" style="margin-top: 150px">
+                    <header class="card-header">
+                        <p class="card-header-title is-centered">Welcome</p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <form id="sign-in-form" method="post" action="{{ route('login') }}">
+                                @csrf
+                                <div class="field">
+                                    <label class="label">Email</label>
+                                    <div class="control has-icons-left has-icons-right">
+                                        <input name="email" class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
+                                               type="email"
+                                               placeholder="Email"
+                                               value="{{ old('email') }}">
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-envelope"></i>
+                                        </span>
+                                        @if($errors->has('email'))
+                                            <span class="icon is-small is-right">
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    @if($errors->has('email'))
+                                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                                    @endif
+                                </div>
+                                <div class="field">
+                                    <label class="label">Password</label>
+                                    <div class="control has-icons-left has-icons-right">
+                                        <input name="password"
+                                               class="input {{ $errors->has('password') ? 'is-danger' : '' }}"
+                                               type="password" placeholder="Password"
+                                               value="{{ old('password') }}">
+                                        <span class="icon is-small is-left">
+                                            <i class="fas fa-envelope"></i>
+                                        </span>
+                                        @if($errors->has('password'))
+                                            <span class="icon is-small is-right">
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    @if($errors->has('password'))
+                                        <p class="help is-danger">{{ $errors->first('password') }}</p>
+                                    @endif
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                        <label class="checkbox">
+                                            <input name="remember"
+                                                   type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                                            @lang("Remember me")
+                                        </label>
+                                    </div>
+                                </div>
+                                <input type="submit" class="is-hidden">
+                            </form>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item" onclick="getElementById('sign-in-form').submit()">Sign In</a>
+                    </footer>
                 </div>
             </div>
-      </div>
-      <div class="new-login-box">
-            <div class="white-box">
-                <h3 class="box-title m-b-0">Sign In</h3>
-                <small>Enter your details below</small>
-                <form class="form-horizontal new-lg-form" id="loginform" method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group m-t-20 {{ $errors->has('email') ? 'has-error' : '' }}">
-                    <div class="col-xs-12">
-                    <label>Email Address</label>
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
-                    @if ($errors->has('email'))
-                        <span class="help-block with-errors">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
-                    <div class="col-xs-12">
-                    <label>Password</label>
-                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Password">
-                    @if ($errors->has('password'))
-                        <span class="help-block with-errors">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-12">
-                    <div class="checkbox checkbox-info pull-left p-t-0">
-                        <input id="checkbox-signup" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label for="checkbox-signup"> {{ __('Remember Me') }} </label>
-                    </div>
-                    <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot password?</a> </div>
-                </div>
-                <div class="form-group text-center m-t-20">
-                    <div class="col-xs-12">
-                    <button class="btn btn-info btn-lg btn-block btn-rounded text-uppercase waves-effect waves-light" type="submit">Log In</button>
-                    </div>
-                </div>
-                </form>
-                <form class="form-horizontal" id="recoverform" action="index.html">
-                <div class="form-group ">
-                    <div class="col-xs-12">
-                    <h3>Recover Password</h3>
-                    <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-                    </div>
-                </div>
-                <div class="form-group ">
-                    <div class="col-xs-12">
-                    <input class="form-control" type="text" required="" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group text-center m-t-20">
-                    <div class="col-xs-12">
-                    <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
-                    </div>
-                </div>
-                </form>
-            </div>
-      </div>            
-</section>
+        </section>
+    </div>
 @endsection
