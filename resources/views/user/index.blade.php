bb6757aaba61f4cfc0dbbd02be9fb427f8200e06
Luis Alcaras <luisalcarasr@gmail.com>
2018-11-26 11:34:41 -0600

    Buefy was installed.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,65 +1,86 @@
 @extends('layouts.main')
 @section('title', 'Usuarios')
 @section('button')
-<a href="{{ route('user.create') }}" class="btn btn-info pull-right m-l-20">
-    <i class="fa fa-plus text-white"></i>
-</a>
+    <a href="{{ route('user.create') }}" class="btn btn-info pull-right m-l-20">
+        <i class="fa fa-plus text-white"></i>
+    </a>
 @endsection
 @section('breadcrumb')
     <li class="active"><a href="{{ route('user.index') }}">Usuarios</a></li>
 @endsection
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="white-box">
-                <div class="table-responsive">
-                    <table class="table table-striped table-improved">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Nombre(s)</th>
-                                <th>Apellido(s)</th>
-                                <th>Rol</th>
-                                <th>Email</th>
-                                <th>Fecha de creación</th>
-                                <th>Fecha de actualización</th>
-                                <th>Fecha de eliminación</th>
-                                <th>Opciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($users as $user)
-                            <tr>
-                                <td>{{ $user->id }}</td>
-                                <td>{{ $user->first_name }}</td>
-                                <td>{{ $user->last_name }}</td>
-                                <td>{{ __('roles.'.$user->roles->first()->name) }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->created_at }}</td>
-                                <td>{{ $user->updated_at }}</td>
-                                <td>{{ $user->deleted_at }}</td>
-                                <td>
-                                    <a class="btn btn-warning" href="{{ route('user.edit', $user->id) }}">
-                                        <i class="fa fa-pencil"></i>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">Usuarios</p>
+            <a href="#" class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                    <i class="fas fa-user-plus" aria-hidden="true"></i>
+                </span>
+            </a>
+        </header>
+        <div class="card-content">
+            <div class="content is-paddingless">
+                <table class="table is-marginless">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre(s)</th>
+                        <th>Apellido(s)</th>
+                        <th>Rol</th>
+                        <th>Email</th>
+                        <th>Creación</th>
+                        <th>Actualización</th>
+                        <th>Eliminación</th>
+                        <th>Opciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->first_name }}</td>
+                            <td>{{ $user->last_name }}</td>
+                            <td>{{ __('roles.'.$user->roles->first()->name) }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->created_at }}</td>
+                            <td>{{ $user->updated_at }}</td>
+                            <td>{{ $user->deleted_at }}</td>
+                            <td>
+                                <div class="field has-addons">
+
+                                <div class="control">
+                                    <a class="button" href="{{ route('user.edit', $user->id) }}">
+                                        <span class="icon is-small">
+                                            <i class="fas fa-pencil"></i>
+                                        </span>
                                     </a>
-                                    <!--<a class="btn btn-primary">
-                                        <i class="fa fa-eye"></i>
-                                    </a>-->
-                                    @if($user->id != Auth::id())
-                                    <a class="btn btn-danger" onclick="event.preventDefault(); document.getElementById('delete-form-{{$user->id}}').submit();">
-                                        <i class="fa fa-{{ $user->trashed() ? 'un' : '' }}lock"></i>
+                                </div>
+                                <div class="control">
+                                    <a class="button">
+                                        <span class="icon is-small">
+                                            <i class="fas fa-eye"></i>
+                                        </span>
                                     </a>
-                                    <form id="delete-form-{{$user->id}}" action="{{ route('user.destroy', $user->id) }}" method="POST" style="display: none;">
-                                        @csrf
-                                        @method('DELETE')
-                                    </form>
-                                    @endif
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                                </div>
+                                @if($user->id != Auth::id())
+                                    <div class="control">
+                                        <a class="button"
+                                           onclick="event.preventDefault(); document.getElementById('delete-form-{{$user->id}}').submit();">
+                                            <i class="fa fa-{{ $user->trashed() ? 'un' : '' }}lock"></i>
+                                        </a>
+                                        <form id="delete-form-{{$user->id}}" action="{{ route('user.destroy', $user->id) }}"
+                                              method="POST" style="display: none;">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                        @endif
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
